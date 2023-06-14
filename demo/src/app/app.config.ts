713980fc6210c9b563206bf0c8db06e863105656
sysmat <tomaz.majerhold@gmail.com>
2023-06-14 16:19:56 +0200

    4 files modified

--- demo/src/app/app.config.ts
+++ demo/src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import {
+  ApplicationConfig,
+  APP_INITIALIZER,
+  ErrorHandler,
+} from '@angular/core';
+import { provideRouter, Router } from '@angular/router';
+import * as Sentry from '@sentry/angular-ivy';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: false,
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+  ],
 };
--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,6 +1,26 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import * as Sentry from '@sentry/angular-ivy';
+import { BrowserTracing } from '@sentry/angular-ivy';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+function initSentry() {
+  Sentry.init({
+    environment: window.location.hostname,
+    dsn: 'https://xxxx',
+    integrations: [
+      new BrowserTracing({
+        tracingOrigins: ['xxx', 'xxxx'],
+        routingInstrumentation: Sentry.routingInstrumentation,
+      }),
+    ],
+    release: 'xxxx',
+    tracesSampleRate: 1.0,
+  });
+}
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+initSentry();
